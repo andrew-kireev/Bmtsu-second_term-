e0f292129475e524ff42ef2c0680e56c56c7e343
Andrew <a-kireev1989@mail.ru>
2019-09-24 09:23:25 +0300

    add

---
+++ Geom_fig/Geom_fig/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Geom_fig
+{
+
+    abstract class Geom_fig 
+    {
+        public abstract double Area();
+    }
+
+    class Rectangle : Geom_fig, IPrint
+    {
+        public Rectangle(double _height, double _lon)
+        { 
+            this.height = _height;
+            this.lon = _lon;
+        }
+        private double height;
+        private double lon;
+        public override double Area () 
+        {
+            return height * lon;
+        }
+        public override string Object.ToString() 
+        {
+            string s = ("Rectangle, S = " + Area().ToString);
+            return s;
+        }
+    }
+
+    class Square : Rectangle, IPrint
+    {
+        private double lonn;
+        public Square (double _lonn) { this.lonn = _lonn; }
+        public override double Area()
+        {
+            return lonn * lonn;
+        }
+
+    }
+
+    class Circle : Geom_fig, IPrint
+    {
+        private double radius;
+        public Circle(double _radius) { this.radius = _radius; }
+        public override double Area()
+        {
+            return radius * radius * 3.14;
+        }
+    }
+
+    interface IPrint 
+    {
+         void Print();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
