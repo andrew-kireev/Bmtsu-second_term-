428bac9d13814c47a1b4c1522d11a62ca03fa602
Andrew <a-kireev1989@mail.ru>
2019-10-25 14:46:32 +0300

    solving some problems

--- Uravnenie_2/Program.cs
+++ Uravnenie_2/Program.cs
@@ -1,83 +1,225 @@
 ﻿using System;
-using System.Numerics;
-
-namespace QuadraticEquations
+namespace Laborotornya_rabota1
 {
-
-
-
-
-
-
     class Program
     {
-        static void Main(string[] args)
+        static void Change_color(int i)
         {
-            // Ввод данных уравнения - значения a, b и c
-            // После ввода данных мы обрезаем лишние пробелы по краям методом Trim()
-            // и преобразовываем тип string во float
-            Console.Write("Введите значение a = ");
-            float a = float.Parse(Console.ReadLine().Trim());
-            Console.Write("Введите значение b = ");
-            float b = float.Parse(Console.ReadLine().Trim());
-            Console.Write("Введите значение c = ");
-            float c = float.Parse(Console.ReadLine().Trim());
-            // Вычисление дискриминанта
-            float d = b * b - 4 * a * c;
-            // При дискриминанте меньшим 0 - выводим ошибку
-            if (d < 0)
+            if (i == 1)
             {
-                float dis = (float)System.Math.Sqrt(d);
-                Complex c2 = new Complex(-b, dis) / 2;
-                Complex c1 = new Complex(-b, -dis) / 2;
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else if (i == 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else if (i == 3)
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+
+        }
+
+
+        static void input(ref double a, ref double b, ref double c)
+        {
+            try
+            {
+                Console.Write("Введите коэффицент a:");
+                a = Convert.ToInt16(Console.ReadLine());
+                Console.Write("Введите коэффицент b:");
+                b = Convert.ToInt16(Console.ReadLine());
+                Console.Write("Введите коэффицент c:");
+                c = Convert.ToInt16(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Change_color(1);
+                Console.WriteLine("Введены некорректные данные");
+                Change_color(3);
+                input(ref a, ref b, ref c);
+            }
+        }
+
+
+
+
+
+        static int Main(string[] args)
+        {
+            double a, b, c;     //коэф
+            double x1, x2, x3, x4;      //Корни 
+
+            a = b = c = 0;
+
+            Console.Title = ("Киреев Андрей Сергеевич ИУ5-33Б");
+
+            if (args.Length == 3)
+            {
+                try
+                {
+                    a = Convert.ToDouble(args[0]);
+                    b = Convert.ToDouble(args[1]);
+                    c = Convert.ToDouble(args[2]);
+                }
+                catch (Exception)
+                {
+                    Change_color(1);
+                    Console.WriteLine("Введены неверные параметры командной строки");
+                    return -1;
+                }
+
             }
             else
             {
-                // Объявляем корни уравнения
-                float x1, x2;
-                float x_1_1, x_1_2, x_2_1, x_2_2;
-                // При дискриминанте равным 0 оба корня равны
-                if (d == 0)
+                input(ref a, ref b, ref c);
+            }
+            if (a != 0 && b != 0 && c != 0)
+            {
+                if (b * b - (4 * a * c) < 0)
                 {
-                    x1 = x2 = -(b / 2 * a);
+                    Change_color(1);
+                    Console.WriteLine("Корней нет");
+                    return 0;
                 }
                 else
                 {
-                    // Извлекаем корень из дискриминанта
-                    float dis = (float)System.Math.Sqrt(d);
-                    // Высчитываем корни уравнения
-                    x1 = (-b + dis) / (2 * a);
-                    if (x1 >= 0) {
-                         x_1_1 = (float)System.Math.Sqrt(x1);
-                         x_1_2 = - (float)System.Math.Sqrt(x1);
-
-                    } else {
-                        Complex c_1_1 = new Complex(0, 1);
-                        c_1_1 = c_1_1 * x1;
-                        Complex c_1_2 = c_1_1;
+                    if ((b * b) - (4 * a * c) == 0)           
+                    {
+                        if (-b / 2 * a < 0)               
+                        {
+                            Change_color(1);
+                            Console.WriteLine("Корней нет");
+                            return 0;
+                        }
+                        else
+                        {
+                            x1 = Math.Sqrt(-b / (2 * a));
+                            x2 = -Math.Sqrt(-b / (2 * a));
+                            Console.WriteLine(x1 + ' ' +x2);
+                            return 0;
+                        }
                     }
-                    x2 = (-b - dis) / (2 * a);
-                    if (x2 >= 0) {
-                         x_2_1 = (float)System.Math.Sqrt(x2);
-                         x_2_2 = -(float)System.Math.Sqrt(x2);
+                    else
+                    {
+                        if ((-b - Math.Sqrt(b * b - 4 * a * c)) / (2 * a) < 0)
+                        {
+                            Change_color(1);
+                            Console.Write("Нет корня x1 и x2");
 
+                        }
+                        else
+                        {
+                            x1 = Math.Sqrt((-b - Math.Sqrt(b * b - 4 * a * c)) 
+                            / (2 * a));
+                            x2 = -Math.Sqrt((-b - Math.Sqrt(b * b - 4 * a * c)) 
+                            / (2 * a));
+                            Change_color(2);
+                            Console.Write("x1 = " + x1.ToString() + " x2 = " + x2.ToString());
+                        }
+                        if ((-b + Math.Sqrt(b * b - 4 * a * c))
+                        / (2 * a) < 0)
+                        {  
+                            Change_color(1);
+                            Console.Write("Нет корня x3 и x4");
 
-                    } else {
-                        //Тут очь тонкий момент, нужно будет потом проверить, тк 
-                        //хз правльно ли комп числа считаются
-                        Complex c_2_1 = new Complex(0, 1);
-                        c_2_1 = c_2_1 * x2;
-                        Complex c_2_2 = c_2_1;
+                        }
+                        else
+                        {
+                            x3 = Math.Sqrt((-b + Math.Sqrt(b * b - 4 * a * c)) 
+                            / (2 * a));
+                            x4 = -Math.Sqrt((-b + Math.Sqrt(b * b - 4 * a * c)) 
+                            / (2 * a));
+                            Change_color(2);
+                            Console.WriteLine(" x3 = " + x3.ToString() + " x4 = "
+                                + x4.ToString());
+                        }
+                        return 0;
                     }
                 }
-                // Выводим результат
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write("d = " + d.ToString() + " x1 = " + x1.ToString() +
-                              " x2 = " + x2.ToString());
             }
-            // Ждем нажатия клавиши, чтобы завершить выполнение программы
-            Console.ResetColor();
-            Console.ReadLine();
+            else
+            {
+                if (a != 0 && b != 0 && c == 0)
+                {
+                    if (b * a > 0)
+                    {
+                        x1 = Math.Sqrt(b / a);
+                        Change_color(2);
+                        Console.WriteLine("x1 = 0");
+                    }
+                    else
+                    {
+                        x1 = Math.Sqrt(-b / a);
+                        Change_color(2);
+                        Console.WriteLine("x1 = 0 x2 = " + x1.ToString() +
+                            " x3 = " + (-x1).ToString());
+                    }
+                }
+                else
+                {
+                    if (a != 0 && b == 0 && c != 0)
+                    {
+                        if (c * a > 0)
+                        {
+                            x1 = Math.Sqrt(c / a);
+                            Change_color(1);
+                            Console.WriteLine("Корней нет");
+                        }
+                        else
+                        {
+                            x1 = Math.Sqrt(Math.Sqrt(-c / a));
+                            Change_color(2);
+                            Console.WriteLine("x1 = " + x1.ToString() +
+                                " x2 = " + (-x1).ToString());
+                        }
+                    }
+                    else
+                    {
+                        if (a != 0 && b == 0 && c == 0)
+                        {
+                            Change_color(2);
+                            Console.WriteLine("Все 4 корня равны нулю");
+                        }
+                        else
+                        {
+                            if (a == 0 && b != 0 && c == 0)
+                            {
+                                Change_color(2);
+                                Console.WriteLine("Два корня равны нулю");
+
+                            }
+                            else
+                            {
+                                if (a == 0 && b != 0 && c != 0)
+                                {
+                                    if (c * b > 0)
+                                    {
+                                        x1 = Math.Sqrt(c / b);
+                                        Change_color(1);
+                                        Console.WriteLine("Корней нет");
+                                    }
+                                    else
+                                    {
+                                        x1 = Math.Sqrt(-c / b);
+                                        Change_color(2);
+                                        Console.WriteLine("x1 = " + x1.ToString() +
+                                            " x2 = " + (-x1).ToString());
+                                    }
+                                }
+                                else
+                                {
+                                    Change_color(2);
+                                    Console.WriteLine("бесконечное решение корней");
+                                }
+                            }
+                        }
+
+                    }
+                }
+
+            }
+            return 0;
         }
     }
 }
